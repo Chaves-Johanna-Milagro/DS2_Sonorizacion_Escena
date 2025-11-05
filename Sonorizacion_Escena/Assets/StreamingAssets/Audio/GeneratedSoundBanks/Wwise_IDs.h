1032598523eb71ebeb1ef9eec9b9f7679d9cb5db
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-11-05 00:05:25 -0300

    Sonidos posicionados
    Se posicionaron algunos de los sonidos como el del globo y las computadoras

--- Sonorizacion_Escena/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Sonorizacion_Escena/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,31 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AMBIENTE2 = 578466671U;
+        static const AkUniqueID PLAY_COMPUTADORA = 3915865825U;
         static const AkUniqueID PLAY_GLOBO = 4069076147U;
         static const AkUniqueID PLAY_METAL_IMPACT_247482 = 2399670526U;
         static const AkUniqueID PLAY_PICAPORTES = 3093474282U;
+        static const AkUniqueID PLAY_STEPS = 2600469463U;
         static const AkUniqueID PLAY_TACHOS = 451097472U;
         static const AkUniqueID PLAY_TAPSPANTALLAS = 3174868516U;
         static const AkUniqueID PLAY_VENTANAS = 645244742U;
-        static const AkUniqueID STOP_GLOBO = 628854749U;
+        static const AkUniqueID STOP_STEPS = 2634345273U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FLOOR_TYPE
+        {
+            static const AkUniqueID GROUP = 4168292868U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LUNE = 677774027U;
+                static const AkUniqueID METAL = 2473969246U;
+            } // namespace SWITCH
+        } // namespace FLOOR_TYPE
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
