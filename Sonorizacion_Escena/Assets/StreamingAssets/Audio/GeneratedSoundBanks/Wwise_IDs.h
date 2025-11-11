57f8342f87422f6ffac217afbcaf627947ef8968
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-11-11 17:21:22 -0300

    Uso nuevos sonidos
    Se cambiaron los sonidos que ya se usaban por los nuevos compuestos en capas, se los posiciono y lopeo dependiendo el objeto, además de integrar la voz a la escena

--- Sonorizacion_Escena/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Sonorizacion_Escena/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,16 +13,17 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ALIEN = 585776263U;
+        static const AkUniqueID PLAY_ALIENDIALOG = 1245305155U;
         static const AkUniqueID PLAY_AMBIENTE2 = 578466671U;
         static const AkUniqueID PLAY_COMPUTADORA = 3915865825U;
         static const AkUniqueID PLAY_GLOBO = 4069076147U;
-        static const AkUniqueID PLAY_METAL_IMPACT_247482 = 2399670526U;
         static const AkUniqueID PLAY_PICAPORTES = 3093474282U;
-        static const AkUniqueID PLAY_STEPS = 2600469463U;
-        static const AkUniqueID PLAY_TACHOS = 451097472U;
+        static const AkUniqueID PLAY_TACHOSNORMALES = 2639633249U;
+        static const AkUniqueID PLAY_TACHOSPELIGROSOS = 116977947U;
         static const AkUniqueID PLAY_TAPSPANTALLAS = 3174868516U;
         static const AkUniqueID PLAY_VENTANAS = 645244742U;
-        static const AkUniqueID STOP_STEPS = 2634345273U;
+        static const AkUniqueID STOP_ALIENDIALOG = 855891249U;
     } // namespace EVENTS
 
     namespace SWITCHES
