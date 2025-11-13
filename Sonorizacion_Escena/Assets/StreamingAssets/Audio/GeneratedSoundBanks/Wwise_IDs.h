2e6cb969d0baa7e14fd35fb0421a4d1a7040f5b4
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-11-13 15:08:41 -0300

    Musica exterior
    Se colocó la musica del exterior en el wwise y se probó en unity

--- Sonorizacion_Escena/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Sonorizacion_Escena/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,6 +17,7 @@ namespace AK
         static const AkUniqueID PLAY_ALIENDIALOG = 1245305155U;
         static const AkUniqueID PLAY_AMBIENTE2 = 578466671U;
         static const AkUniqueID PLAY_COMPUTADORA = 3915865825U;
+        static const AkUniqueID PLAY_EXTERIOR = 2890489828U;
         static const AkUniqueID PLAY_GLOBO = 4069076147U;
         static const AkUniqueID PLAY_PICAPORTES = 3093474282U;
         static const AkUniqueID PLAY_TACHOSNORMALES = 2639633249U;
